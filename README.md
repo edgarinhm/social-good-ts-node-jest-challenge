76791b96f713de0b5b98faebbbd65b89663c99c6
Edgar Mejia <edgarinhm@hotmail.com>
2025-02-11 16:37:57 -0500

    refactor file class arrow functions add readme file

---
+++ README.md
@@ -0,0 +1,24 @@
+# Welcome node manage file to replace version tag!
+
+
+## Getting Started
+
+### Installation
+Install the dependencies:
+
+```bash
+npm install
+```
+
+### Development
+
+Transpile typescript files:
+
+```bash
+npm run compile
+```
+
+### Test
+```bash
+npm run test
+```
--- files.ts
+++ files.ts
@@ -2,45 +2,43 @@ const path = require("path");
 const fs = require("fs");
 const { promises: fsPromises } = fs;
 
-class Files {
+export class Files {
   encoding = "";
   FILES_DIR = "/";
   constructor(encoding = "utf8") {
     this.encoding = encoding;
   }
 
-  public readFile = async (fileName: string) => {
+  public async readFile (fileName: string) {
     return await fsPromises.readFile(
       path.join(__dirname, this.FILES_DIR, fileName),
       this.encoding
     );
   };
 
-  public createFile = async (fileName: string, content: string) => {
+  public async createFile(fileName: string, content: string)  {
     return await fsPromises.writeFile(
       path.join(__dirname, this.FILES_DIR, fileName),
       content
     );
   };
 
-  public updateFile = async (fileName: string, content: string) => {
+  public async updateFile(fileName: string, content: string)  {
     return await fsPromises.appendFile(
       path.join(__dirname, this.FILES_DIR, fileName),
       content
     );
   };
 
-  public deleteFile = async (fileName: string) => {
+  public async deleteFile (fileName: string)  {
     return await fsPromises.unlink(
       path.join(__dirname, this.FILES_DIR, fileName)
     );
   };
 
-  public copyFile = async (orignPath: string, targetPath: string) => {
+  public async copyFile (orignPath: string, targetPath: string)  {
     const readStream = fs.createReadStream(orignPath);
     const writeStream = fs.createWriteStream(targetPath);
     readStream.pipe(writeStream);
   };
 }
-
-export { Files };
